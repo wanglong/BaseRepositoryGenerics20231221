d6f7e2df82d8e8d4797f7a267b6521c7de1e922f
Leo <lowang@mycwt.com>
2023-12-21 14:30:34 +0800

    test

---
+++ WebAPI.Net6.BaseRepositoryGenerics/WebAPI.Net6.BaseRepositoryGenericsTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI.Net6.BaseRepositoryGenerics.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Net6.BaseRepositoryGenerics.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
