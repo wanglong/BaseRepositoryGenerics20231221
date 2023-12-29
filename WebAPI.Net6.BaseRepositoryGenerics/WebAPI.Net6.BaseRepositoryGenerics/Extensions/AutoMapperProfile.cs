ab2c83d4d24a36770093b9a3db4a0c99a5822ed9
leo <LoWang@mycwt.com>
2023-12-29 22:52:30 +0800

    refactoring code!

---
+++ WebAPI.Net6.BaseRepositoryGenerics/WebAPI.Net6.BaseRepositoryGenerics/Extensions/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebAPI.Net6.BaseRepositoryGenerics.Application.ErrorMessage.Common;
+using WebAPI.Net6.BaseRepositoryGenerics.Domain.Models;
+
+namespace WebAPI.Net6.BaseRepositoryGenerics.Extensions
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            this.CreateMap<TErrorMessage, ErrorMessageViewModel>();
+        }
+    }
+}
---
+++ WebAPI.Net6.BaseRepositoryGenerics/WebAPI.Net6.BaseRepositoryGenerics/Extensions/AutofacModuleRegister.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using System.Reflection;
+
+namespace WebAPI.Net6.BaseRepositoryGenerics.Extensions
+{
+    public class AutofacModuleRegister : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // 服务项目程序集
+            Assembly shoppingServiceAssembly = Assembly.Load("CWT.BookingEngine.ShoppingService");
+
+            // 注册UnitOfWork
+            builder.RegisterAssemblyTypes(shoppingServiceAssembly)
+                .Where(n => n.FullName.EndsWith("UnitOfWork") && !n.IsAbstract)
+                .InstancePerLifetimeScope() // 瞬时单例 .InstancePerDependency()
+                .AsImplementedInterfaces() // 自动以其实现的所有接口类型暴露（包括IDisposable接口）
+                .EnableInterfaceInterceptors(); // 引用Autofac.Extras.DynamicProxy;
+
+            // 注册Repository
+            builder.RegisterAssemblyTypes(shoppingServiceAssembly)
+                .Where(n => n.FullName.EndsWith("Repository") && !n.IsAbstract)
+                .InstancePerLifetimeScope() // 瞬时单例 .InstancePerDependency()
+                .AsImplementedInterfaces() // 自动以其实现的所有接口类型暴露（包括IDisposable接口）
+                .EnableInterfaceInterceptors(); // 引用Autofac.Extras.DynamicProxy;
+
+            // 注册Service
+            builder.RegisterAssemblyTypes(shoppingServiceAssembly)
+                .Where(n => n.FullName.EndsWith("Service") && !n.IsAbstract)
+                .InstancePerLifetimeScope() // 瞬时单例 .InstancePerDependency()
+                .AsImplementedInterfaces() // 自动以其实现的所有接口类型暴露（包括IDisposable接口）;
+                .EnableInterfaceInterceptors(); // 引用Autofac.Extras.DynamicProxy;
+        }
+    }
+}
