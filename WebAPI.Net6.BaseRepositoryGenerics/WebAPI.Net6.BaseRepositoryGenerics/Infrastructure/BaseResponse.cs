6060d532bc1dfe193c49c3a98a347dd5cb0d61c3
leo <LoWang@mycwt.com>
2023-12-29 22:54:50 +0800

    modify code,autofac add module register!

---
+++ WebAPI.Net6.BaseRepositoryGenerics/WebAPI.Net6.BaseRepositoryGenerics/Infrastructure/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace WebAPI.Net6.BaseRepositoryGenerics.Infrastructure
+{
+    public partial class BaseResponse
+    {
+
+        /// <summary>
+        /// Gets or sets the trace identifier.
+        /// </summary>
+        /// <value> The trace identifier. </value>
+        [JsonProperty("traceId")]
+        public string TraceId { get; set; } = Guid.NewGuid().ToString();
+    }
+}
